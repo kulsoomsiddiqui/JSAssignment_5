7303d57b0512b6611292d4fb332e51cb200358ef
Umm e Kulsoom Siddiqiui <58472724+kulsoomsiddiqui@users.noreply.github.com>
2020-06-14 16:59:17 +0500

    Add files via upload

---
+++ JavaScript Assignment/app.js
@@ -0,0 +1,535 @@
+// Chap1 Quetion 01
+alert("Welcome To Our Website");
+// Questin 02
+alert("Error! Please Enter a vlaid valid");
+// Question 03
+alert("Welcome to JS Land.................. \n Happy Coding");
+// Question 04
+alert("Welcome to JS Land.....");
+alert("Happy Coding :) !!!!!!!!!");
+// Question 05
+// for using using console.............
+// Question 06
+alert("Welcome to iour JS land hope uh like it");
+// Question 07
+alert("I am React Developer");
+// <title>js</title>
+// <script>
+//     alert("I am React Developer");//head
+// </script>
+// </head>
+// <script>
+// alert("I am React Developer");//befor body
+// </script>
+
+// <body>
+// <script>
+//     alert("I am React Developer");//inside body
+// </script>
+
+// </body>
+// <script>
+// alert("I am React Developer");//after
+// </script>
+// </html>
+// Chap 02 Question 08
+var username;
+// Question 09
+var Myname = "Muzna Ali";
+// Question 10
+var massage = "Hello World"
+alert(massage);
+// Question 11
+var Students = "Jhon Doe";
+var age = "15 Years Old";
+var Coures = "Certified Mobile Application Development ";
+alert(Students);
+alert(age);
+alert(Coures);
+// Question 12
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+// Question 13
+var Myaddress = "My Email Is"
+var Email = "example@example.com";
+alert(Myaddress + Email);
+// Question 14
+var book = "A smarter way to learn JavaScript"
+alert(" i m tring to learn from the BOOK " + book);
+// Question 15
+var display = "Yah! I can write HTML content through JavaScript";
+document.write(display);
+// Question 16
+var pattern = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(pattern);
+// Chap 03 Question 17
+var age = 15;
+alert = ("I am " + age + "Years Old");
+// Question 18
+var Trackes_times = "14";
+alert("You have visited this " + rackes_times + "times");
+// Question 19
+var birth_Year = 1990;
+document.write("My birth Year is " + birth_Year + "\n Data Type of my declared variable is number ");
+var visitors_name = "John Doe";
+var Product_title = " Clothing Store";
+var Quantity = "5 T-shirt";
+//  CHAP 04 QUESTION 20
+var university = "Saylani Mass It Training",
+    validity = "augest,2020",
+    address = "Head Office bahudrabad";
+// Question 21
+var firstName;
+var secondName;
+var thirdName;
+var fourtName;
+var fifthName;
+
+// Question 22
+document.write("Rules For Variable JS variables"); <
+br / >
+    document.write("Variable names can only contain number,$,and _.For example $my_1stVariable \n Variables must begin with a letter, $ or _.For example $name, _name or name \n Variable names are case  sesitive \n Variable names should not be JS keywords");
+// Question 23
+var a = 3;
+var b = 5;
+var sum = a + b;
+document.write("Sum of " + a + "And" + b + "is " + sum + " ");
+//Question 24
+var a = 3;
+var b = 5;
+var sub = a - b;
+document.write("Sub of " + a + "And" + b + "is " + sub + " "); < br / >
+    var a = 3;
+var b = 5;
+var Mult = a * b;
+document.write("mul of " + a + "And" + b + "is " + mul + " "); < br / >
+    var a = 3;
+var b = 5;
+var Divi = a / b;
+document.write("div of " + a + "And" + b + "is " + div + " "); < br / >
+    var a = 3;
+var b = 5;
+var Mod = a % b;
+document.write("Mod of " + a + "And" + b + "is " + mod + " ");
+// Question 25
+var num;
+document.write("Value after variable declaration is: " + num + "<br/>");
+num = 5;
+document.write("Initial  value : " + num + "<br/>");
+num = num + 1;
+document.write("Value after increment is:" + num + "<br/>");
+num = num + 7;
+document.write("Value after addition is: " + num + "<br/>");
+num = num - 1;
+document.write("Value after decrement is: " + num + "<br/>");
+num = num % 3;
+document.write("The reminder is: " + num + "<br/>");
+//Question 26
+var cost_of_one_Movie_ticket = 600;
+var cost_of_one_BUYING_ticket = 5;
+var total = cost_of_one_Movie_ticket * cost_of_one_BUYING_ticket;
+document.write("Total cost of buy " + cost_of_one_BUYING_ticket + "tickets  to a movie is " + total + "PKR");
+// question 27
+document.write("Table of 4");
+document.write("4*1=4 \n 4*2=8 \n4*3=12\n4*4=16 \n4*5=20 \n4*6=24 \n4*7=28\n4*8=32\n4*9=36\n4*10=30");
+// Question 28
+document.write("Mark sheet");
+var total_Marks = 980;
+var Marks_obtained = 804;
+var percentage = Marks_obtained * total_Marks / 100;
+document.write("" + Marks_obtained + "\n" + total_Marks + "\n" + percentage + "");
+// Question 29
+var us_dollar = 10;
+var excha_rates_for_each = 104.80;
+var saudi_riyal = 25;
+var exchang_rates_for_each = 28;
+var single_expre = us_dollar * excha_rates_for_each;
+var sing_expre = saudi_riyal * exchang_rates_for_each;
+var total = sing_expre + single_expre;
+document.write(" Currency In PKR\nTotal Currency in PKR: " + total + " ");
+// Question 30
+var a = 3;
+var b = ((3 + 5) * 10) / 2;
+document.write("<br/>" + b + "");
+// Question 31
+var cur_year = 2016;
+var birth_year = 1992;
+var your_age = cur_year - birth_year;
+document.write("<br/>Current Year: " + cur_year + "<br/> Birth Year: " + birth_year + "<br/>Your Age: " + your_age + "");
+// Question 31
+var radius = 20;
+var circumference_of_circle = 2 * (3.142) * radius;
+var area_of_a_circle = (3.142) * radius * radius;
+document.write("<br/>Radus of a circle" + radius + "<br/>The circunference is: " + circumference_of_circle + "<br/>The area is: " + area_of_a_circle + " ");
+// Question 32
+var snakes = "chocolate chips";
+var age = "15";
+var max_age = "65";
+var amnt_per_day = 3;
+var total = (max_age - age) * amnt_per_day;
+document.write("<br/>Favourite Snakes: " + snakes + "<br/>Current age: " + age + "<br/> Maximum Age: " + max_age +
+    "<br/>Amount of snacks per day:" + amnt_per_day + "<br/>You will need " + total + " " + snakes + "to last you until the ripe olg age of " + max_age + "");
+// chap 06 to 09 Question 33
+var a = 10;
+document.write("<br/>Result: <br/> The value of a is: " + a + " <br/>..............." +
+    "<br/><br/>The value of ++a  is: " + (++a) +
+    " <br/> Now the  value of a is: " + a + "<br/><br/>The value of a++  is: " + (a++) +
+    " <br/> Now the  value of a is: " + a + "<br/><br/>The value of --a  is: " + (--a) +
+    " <br/> Now the  value of a is: " + a + "<br/><br/>The value of a--  is: " + (a--) +
+    " <br/> Now the  value of a is: " + a + "<br/>");
+// Question 34
+var a = 2,
+    b = 1;
+var result = --a - --b + ++b + b--;
+document.write("a is ??" + a + "<br/>b is ??" + b + "<br/>result is??" + result + "<br>");
+// question 35
+var username = prompt("Enter Your Name:");
+alert("Welcome " + username + " Good to see You hope u like it :)")
+    // Question 36
+var table = prompt("Enter A Number For A table", "5");
+var m = Number(table);
+var num = 1;
+document.write(
+    m + " x " + num + " = " + (m * num++) + "<br/>" +
+    m + " x " + num + " = " + (m * num++) + "<br/>" +
+    m + " x " + num + " = " + (m * num++) + "<br/>" +
+    m + " x " + num + " = " + (m * num++) + "<br/>" +
+    m + " x " + num + " = " + (m * num++) + "<br/>" +
+    m + " x " + num + " = " + (m * num++) + "<br/>" +
+    m + " x " + num + " = " + (m * num++) + "<br/>" +
+    m + " x " + num + " = " + (m * num++) + "<br/>" +
+    m + " x " + num + " = " + (m * num++) + "<br/>" +
+    m + " x " + num + " = " + (m * num++) + "<br/>"
+);
+// Question 37
+var subj1 = prompt("Enter A First Number")
+var subj2 = prompt("Enter A Second Number");
+var subj3 = prompt("Enter A Third Number");
+var subj1marks = prompt("Enter A First Subject Marks");
+var a = Number(subj1marks)
+var subj2marks = prompt("Enter A Second Subject Marks");
+var b = Number(subj2marks)
+var subj3marks = prompt("Enter A Third Subject Marks");
+var c = Number(subj3marks)
+var totalmarks = 100
+document.write(
+    "<table>" +
+    "<tr><th>Subject</th><th>Total Mark</th><th>Obtained Marks</th><th>Percentage</th></tr>" +
+    "<tr><td>" + subj1 + "</td><td>" + totalmarks + "</td><td>" + a + "</td><td>" + (a / totalmarks * totalmarks) + "%</td><tr>" +
+    "<tr><td>" + subj2 + "</td><td>" + totalmarks + "</td><td>" + b + "</td><td>" + (b / totalmarks * totalmarks) + "%</td><tr>" +
+    "<tr><td>" + subj3 + "</td><td>" + totalmarks + "</td><td>" + c + "</td><td>" + (c / totalmarks * totalmarks) + "%</td><tr>" +
+    "<tr><td></td><td>" + (totalmarks * 3) + "</td><td>" + (a + b + c) + "</td><td>" + ((a + b + c) / (totalmarks * 3) * 100) + "%</td><tr>" +
+    "</table>"
+);
+//  chap 9 to 11 Quesyion 38
+var city = prompt("Enter your city Name");
+var city = "karachi";
+if (city === city) {
+    alert("welcome to city of lights:)")
+}
+// Question 39
+var gender = prompt("enter your gender plzz!");
+var gender1 = "male";
+var gender2 = "Female";
+if (gender1 === gender1) {
+    alert("Good Morning Sir");
+}
+if (gender2 === gender2);
+alert("Good Morning Maam");
+// Question 40
+var color = prompt("Enter A Signal Color!");
+var color1 = "Red";
+
+if (color1 === color1); {
+    alert("Must Stop");
+}
+var color2 = "Yellow";
+if (color2 === color2); {
+    alert("Ready To Move ");
+}
+
+var color3 = "Green";
+if (color3 === color3); {
+    alert("Move Now");
+}
+//    Question 41
+
+var fuel = prompt("Please cheak your Fuel")
+if (fuel <= 0.25) {
+    alert("Please refill the fuel in your car ")
+} else {
+    alert("Enjoy your journey")
+}
+//   Question 42
+var a = 4;
+if (++a === 5) { alert("given condition for variable a is true"); }
+var b = 82;
+if (b++ === 83) { alert("given condition for variable b is true"); }
+var c = 12;
+if (c++ === 13) { alert("condition 1 is true"); }
+if (c === 13) { alert("condition 2 is true"); }
+if (++c < 14) { alert("condition 3 is true"); }
+if (c === 14) { alert("condition 4 is true"); }
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost) { alert("The cost equals"); }
+if (true) { alert("True"); }
+if (false) { alert("False"); }
+if ("car" < "cat") { alert("car is smaller than cat"); }
+// Question 43
+alert("WellCome to  JS  MarksSheet")
+var sub1 = prompt("Enter A First Subject Number")
+var a = Number(sub1)
+var sub2 = prompt("Enter A Second Subject Number")
+var b = Number(sub2)
+var sub3 = prompt("Enter A Third Subject Number")
+var c = Number(sub3)
+var total_m = 300
+var markobtained = a + b + c;
+var percen = markobtained / total_m * 100
+if (percen >= 80) {
+    grade = "A-one";
+    remarks = "Excellent";
+}
+if (percen >= 70) {
+    grade = "A";
+    remarks = "Good";
+}
+if (percen >= 60) {
+    grade = "B";
+    remarks = "You need to improve";
+}
+if (percen < 60) {
+    grade = "F";
+    remarks = "Soory"
+}
+document.write(
+    "<h1>MarkSheet</h1>" +
+    "<br/>Total Marks :" + total_m +
+    "<br/>Markobtained :" + markobtained +
+    "<br/>Percentage :" + percen +
+    "<br/>Grade :" + grade +
+    "<br/>Remarks :" + remarks +
+    "<br/>"
+);
+//    Question 44
+var num = 6;
+num = prompt("Guesses The  Secert Number ")
+if (num == 6) {
+    alert("Bingo!Coreect The Answer")
+}
+if (num !== 6) {
+    alert("Close enough to the correct answer")
+}
+// Question 45
+var num = 9;
+num = prompt("Enter A Number")
+if (num % 3 === 0) {
+    alert("This number is divisible by 3")
+}
+if (num % 3 !== 0) { alert("This number is not divisible by 3") }
+//    Question 46
+var num = 10;
+num = prompt("Enter A  Number")
+if (num % 2 === 0) { alert("Given input is even number") }
+var num1 = 9;
+if (num1 % 3 === 0) { alert("Given input is Odd number") }
+//  Question 47
+var temp = prompt("Enter your temperature and get your results:)")
+if (temp > 40) { alert("Its too hot outside.") }
+if (temp > 30) { alert("The Weather today is Normal.") }
+if (temp > 20) { alert("Today's weather is  Cool.") }
+if (temp > 10) { alert("OMG!Today's weather is so Cool.") }
+//    Question 48
+alert("Welcome to JS calculator")
+Number1 = prompt("Number1")
+Number2 = prompt("Number2")
+operator = prompt("+ - / * % ");
+num1 = Number(Number1);
+num2 = Number(Number2);
+if (operator == "+") {
+    ans = num1 + num2;
+    alert(ans)
+}
+if (operator == "-") {
+    ans = num1 - num2;
+    alert(ans)
+}
+if (operator == "/") {
+    ans = num1 / num2;
+    alert(ans)
+}
+if (operator == "*") {
+    ans = num1 * num2;
+    alert(ans)
+}
+if (operator == "%") {
+    ans = num1 % num2;
+    alert(ans)
+}
+// Question 49
+input = prompt("Enter any number or character")
+if (Number(input)) { alert("Your input is a Number") } else if (input >= "a" && input <= "z") { alert("Your input is a Lower case") } else if (input >= "A" && input <= "Z") { alert("Your input is a Upper case") } else { alert("Check  your Char") };
+// Question 50
+var num1 = +prompt("Enter first integer")
+var num2 = +prompt("Enter second  integer")
+if (num1 > num2) { alert(num1 + "is larger than" + num2) } else if (num1 < num2) { alert(num2 + "is larger than" + num1) } else { alert(num1 + "and" + num2 + "are equal") }
+// Question 51
+input = prompt("Enter any character")
+if (input == "a" || input == "e" || input == "i" || input == "o" || input == "u") { alert("true It's a vowel") } else { alert("false  its not a vowels") }
+// Question 51
+input = prompt("takes input from user")
+if (input > 0) {
+    alert("The number is positive;)")
+} else if (input < 0) {
+    alert("The number is negative;)")
+} else if (input == 0) {
+    alert("The number is Zero;)")
+} else {
+    alert("Try again:(")
+}
+// Question 52
+var password = "abc5909";
+var userpassword = prompt("Enter your password");
+if (userpassword === null || userpassword === "") {
+    alert("Please enter your password");
+} else if (userpassword === password) {
+    alert("Correct! The password you entered matches the original password");
+} else {
+    alert("Incorrect password");
+}
+// Question 53
+var greeting;
+var hour = 13;
+if (hour < 18) {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+// Question 54
+var time = +prompt("What Time is it?")
+if (time >= 0000 && time < 1200) {
+    alert("GOOD MORNING")
+}
+if (time >= 1200 && time < 1700) {
+    alert("GOOD AFTERNOON")
+}
+if (time >= 1700 && time < 2100) {
+    alert("GOOD EVENING")
+}
+if (time >= 2100 && time <= 2359) {
+    alert("GOOD NIGHT")
+}
+// Question 55
+// <!-- Declare an empty array using JS literal notation to storestudent names in future. -->
+var studentname = [];
+//   <!--Declare an empty array using JS object notation to store student names in future. -->
+var names = {};
+//   <!--  Declare and initialize a strings array. -->
+var strings = "Muzna";
+//   <!-- Declare and initialize a numbers array -->
+var numbers = ["1,2,3,4,5"];
+//   <!-- Declare and initialize a boolean array. -->
+var booleans = ["true,false"];
+//   <!-- Declare and initialize a mixed array. -->
+var mixed_array = ["Muzna", true, false, 1, 6, 8];
+//   Declare and Initialize an array and store available education qualifications in Pakistan
+//   Question 56
+var education_qualif_in_Pak = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"];
+document.write("<h1>Qualification:</h1>");
+document.write("1 " + education_qualif_in_Pak[0] + "<br/>" + "2)" +
+    education_qualif_in_Pak[1] + "<br/>" + "3)" + education_qualif_in_Pak[2] + "<br/>" + "4)" +
+    education_qualif_in_Pak[3] + "<br/>" + "5)" + education_qualif_in_Pak[4] + "<br/>" + "6)" +
+    education_qualif_in_Pak[5] + "<br/>" + "7)" + education_qualif_in_Pak[6] + "<br/>" + "8)" +
+    education_qualif_in_Pak[7] + "<br/>");
+
+//  Question 57
+var studentname = ["Michael", "John", "Tony"];
+var store_score = [320, 230, 480];
+var total_marks = 500;
+document.write("Score of " + studentname[0] + "is" + store_score[0] + ".Percentage" +
+    store_score[0] / total_marks * 100 + "%<br/>");
+document.write("Score of " + studentname[1] + "is" + store_score[1] + ".Percentage" +
+    store_score[1] / total_marks * 100 + "%<br/>");
+document.write("Score of " + studentname[2] + "is" + store_score[2] + ".Percentage" +
+    store_score[2] / total_marks * 100 + "%<br/>");
+
+// <!--Initialize an array with color names. Display the array elements in your browser-->
+// Question 58
+var color_names = ["Black", "Blue", "Brown"];
+alert("ColorName:", "<p>" + color_names);
+document.write(color_names + "<p>");
+// Ask the user what color he/she wants to add to the beginning & add that color to the beginning of the array.Display the updated array in your browser.
+var add_color = prompt("Which color do you want to add at the beginning");
+color_names.unshift(add_color);
+alert(color_names);
+document.write(color_names + "<p>");
+//  Ask the user what color he/she wants to add to the end & add that color to the end of the array. Display the updated array in your browser. 
+var updated_color = prompt("Which color do you want to add to the end");
+color_names.push(updated_color);
+alert(color_names);
+document.write(color_names + "<p>");
+//  Add two more color to the beginning of the array. Display the updated array in your browser. 
+var add_another_color = prompt("Which color do you want to add two more color");
+color_names.splice(0, 0, "mehroon", "red");
+alert(color_names);
+document.write(color_names + "<p>");
+//  Delete the last color in the array. Display the updated array in your browser. 
+var delete_color = prompt("Which color do you want delete to at the beginning");
+color_names.shift(delete_color);
+alert(color_names);
+document.write(color_names + "<p>");
+// Ask the user at which index he/she wants to add a color & color name. Then add the color to desired position/index.
+//  . Display the updated array in your browser. g
+//  Ask the user at which index he/she wants to delete color(s) & how many colors he/she wants to delete. Then 
+var update_color = prompt("Which color do you want add a color with their position");
+var index_color = prompt("Which color do you want delete color & how many");
+color_names.splice(2, 2);
+alert(color_names);
+document.write(color_names + "<p>");
+// Question 58
+var Score_of_Stud = [320, 230, 480, 120];
+document.write("Score of Students :" + Score_of_Stud + "<br/>");
+Score_of_Stud.sort();
+document.write(" Order Score of Students :" + Score_of_Stud + "<br/>");
+// Question 59
+var Cities_list = ["Karachi", "Lahore", "Islamabad", "Quetta", "Pershawar"];
+var selected_List = Cities_list.slice(2, 4);
+document.write("Cities List:" + "<br/>" + Cities_list + "<br/>");
+document.write("Selected Cities List: " + "<br/>" + selected_List);
+// Question 60
+var array = ["This", "is", "my", "cat"];
+var single_array = array.join("");
+document.write("Array:<br>" + array + "<br/>");
+document.write("String:<br>" + array + "<br/>");
+// question 61
+var device = ["Keyboard", "Mouse", "Printer", "Monitor"]
+document.write("Devices:<br/>" + device + "<br/><p>");
+var out = device.shift();
+document.write("Out:<br/>" + out + "<br/>");
+var out = device.shift();
+document.write("Out:<br/>" + out + "<br/>");
+var out = device.shift();
+document.write("Out:<br/>" + out + "<br/>");
+var out = device.shift();
+document.write("Out:<br/>" + out + "<br/>");
+// Question 62
+var device = ["Keyboard", "Mouse", "Printer", "Monitor"]
+document.write("Devices:<br/>" + device + "<br/><p>");
+var out = device.pop();
+document.write("Out:<br/>" + out + "<br/>");
+var out = device.pop();
+document.write("Out:<br/>" + out + "<br/>");
+var out = device.pop();
+document.write("Out:<br/>" + out + "<br/>");
+var out = device.pop();
+document.write("Out:<br/>" + out + "<br/>");
+// Question 63
+var phone_manufacturers = ["Apple", "Samsung", "Motrola", "Nokia", "Sony", "Haier"];
+document.write("<select><option>" + phone_manufacturers[0] + "</option>" +
+    "<option>" + phone_manufacturers[1] + "</option>" +
+    "<option>" + phone_manufacturers[2] + "</option>" +
+    "<option>" + phone_manufacturers[3] + "</option>" +
+    "<option>" + phone_manufacturers[4] + "</option>" +
+    "<option>" + phone_manufacturers[5] + "</option>" +
+    "</select>");
